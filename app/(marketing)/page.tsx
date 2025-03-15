19e5e1aaafe24b0fef40f7170d1f980cc469bd81
sulthonkaf <sulthonkaf@gmail.com>
2025-03-15 13:02:10 +0000

    teams

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -6,12 +6,4 @@ const LandingPage = dynamic(() => import("@/components/landing-page"), {
 
 export default function Home() {
   return <LandingPage />
-}import dynamic from "next/dynamic"
-
-const LandingPage = dynamic(() => import("@/components/landing-page"), {
-  ssr: false,  // Nonaktifkan SSR untuk rendering sisi klien
-})
-
-export default function Home() {
-  return <LandingPage />
 }
